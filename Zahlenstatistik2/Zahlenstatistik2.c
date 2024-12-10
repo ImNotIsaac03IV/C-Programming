d7fe6cbbd30b48651568ad61d2d16c0a12d19a81
ImNotIsaac03IV <isaac.darko04@outlook.com>
2024-12-10 09:44:58 +0100

    Finished project

--- Zahlenstatistik2/Zahlenstatistik2.c
+++ Zahlenstatistik2/Zahlenstatistik2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() 
 {
@@ -6,23 +7,44 @@ int main()
 	int sum = 0;
 	int counter = 0;
 	float average;
-	int max = 0;
-	int min = 0;
+	int max1 = INT_MIN;
+	int min = INT_MAX;
+	int max2 = INT_MIN;
 	printf("Insert number (0 to exit): ");
 	scanf_s("%d", &input);
-	input = min;
+
 
 	while (input != 0) 
 	{
-		if(input % 2 == 1)
+		if(input % 2 == 1)	
 		{
 			sum += input;
 			counter++;
 		}
-		average = float(sum / counter);
-		
+
+		if (input < min)
+		{
+			min = input;
+		}
+
+		if (input > max1)
+		{
+			max2 = max1;
+			max1 = input;
+		}
+		else if (input > max2 && input < max1) 
+		{
+			max2 = input;
+		}
+
 		printf("Insert number (0 to exit): ");
 		scanf_s("%d", &input);
 	}
+
+	average = (float)sum / (float)counter;
+	printf("Average: %.2f\n", average);
+	printf("Biggest number: %d\n", max1);
+	printf("Second biggest number: %d\n", max2);
+	printf("Smallest number: %d\n", min);
 	return 0;
 }
