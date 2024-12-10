4228f6248c749cfee5d2f1bea68369094ac9e33e
ImNotIsaac03IV <isaac.darko04@outlook.com>
2024-12-10 09:55:18 +0100

    Erased test program

--- Playground/Playground.c
+++ Playground/Playground.c
@@ -2,24 +2,6 @@
 
 int main() 
 {
-    int num;
-    printf("Choose an option: ");
-    scanf_s("%d", &num);
 
-    switch(num)
-    {
-        case 1:
-            printf("Open menu\n");
-            break;
-        case 2:
-            printf("Close menu\n");
-            break;
-        case 3:
-            printf("Next page\n");
-            break;
-        default:
-            printf("Non-existent\n");
-            break;
-    }
     return 0;
 }
